7fd251d26ed01387d905ed37428fc7cbf4edcfd0
José Acerbi <joseacerbialmeidaneto@hotmail.com>
2025-04-30 20:00:28 -0300

    feat: desenvolvido funcao para listar reviews dos livros

--- src/services/booksApi.ts
+++ src/services/booksApi.ts
@@ -33,3 +33,27 @@ export const getBestSellers = async ({
     throw error;
   }
 };
+
+interface GetReviewsParams {
+  author?: string;
+  title?: string;
+  isbn?: string;
+}
+
+export const getBookReviews = async ({ author, title, isbn }: GetReviewsParams) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/reviews.json`, {
+      params: {
+        'api-key': API_KEY,
+        author,
+        title,
+        isbn
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao buscar resenhas:', error);
+    throw error;
+  }
+};
