8e0a7aae1016d613d0857401e468dcb5b23f4fdd
José Acerbi <joseacerbialmeidaneto@hotmail.com>
2025-05-07 09:14:23 -0300

    feat: estilizando interface

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,11 +10,6 @@ const Footer = () => {
           <p className="copyright">
             &copy; {currentYear} Zetta Lab - Books API. All rights reserved.
           </p>
-          <div className="footer-links">
-            <a href="#" className="footer-link">Terms of Service</a>
-            <a href="#" className="footer-link">Privacy Policy</a>
-            <a href="#" className="footer-link">Contact</a>
-          </div>
         </div>
       </div>
     </footer>
